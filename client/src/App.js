2e9b1f463b61d9ecb4df7776db947d010ed65163
Madison Haradine <madisonharadine@gmail.com>
2020-02-18 22:21:08 -0500

    navbar layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import NavBar from './components/NavBar'
 
 import styles from './app.module.css'
 
 function App() {
   return (
     <div className={styles.container}>
+      <NavBar />
       My workout tracker
       <div className={styles.box}>color1</div>
       <div className={styles.box2}>color2</div>
+      <div className={styles.box3}>box3</div>
     </div>
   );
 }
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import styles from './navbar.module.css'
+
+const NavBar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoBox}>
+        logo
+      </div>
+      <div className={styles.date}>
+        Tuesday, February 40th
+      </div>
+      <div className={styles.linksBox}>
+        <div className={styles.link}>
+          my account
+        </div>
+        <div className={styles.link}>
+          log out
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
